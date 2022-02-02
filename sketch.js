f1efa6201b866b2c72599b84dbbd5e694c80fa40
Virgil Flack <97695026+VirgilFlack@users.noreply.github.com>
2022-02-01 21:56:51 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup(){
+   createCanvas(800,800)
+   background(1,1,1);
+    
+// The Swedish FLag
+fill(300, 1, 1)
+stroke(500, 0, 0);
+rect(360, 200, 400, 200)
+fill(300, 300, 300)
+stroke(500, 500, 500);
+rect(359.9, 280, 400, 40)
+// The Swedish Flag
+fill(300, 300, 300)
+stroke(500, 500, 500);
+rect(450, 200, 40, 200)
+// The Cangolese Flag
+fill(300, 300, 0)
+stroke(300, 300, 0);
+rect(359.9,450, 400, 200)
+//
+fill(65,146,77);
+stroke(65,146,77);
+triangle(360, 650, 600, 450, 359, 450)
+// 
+fill(300, 1, 1);
+stroke(500, 0, 0);
+triangle(545, 650, 760, 450, 760, 650)
+//The Cangolese Flag
+
+}
